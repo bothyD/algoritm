4ed22f07a4a22644ffe1037170a5b97a58695011
bothy <pavla.acc@mail.ru>
2023-03-06 03:44:27 +0700

    add alg for basis file lab2

--- lab2.py
+++ lab2.py
@@ -18,50 +18,6 @@ class fractions:
         if(first_chisl%first_zn == 0):
             return str(int(first_chisl/first_zn))
         else:
-            
-
-            # while (first_chisl%2==0 and first_zn%2==0):
-            #     first_chisl/=2
-            #     first_zn/=2
-            # while(first_chisl%3==0 and first_zn%3==0):
-            #     first_chisl/=3
-            #     first_zn/=3
-            # while(first_chisl%5==0 and first_zn%5==0):
-            #     first_chisl/=5
-            #     first_zn/=5
-            # while(first_chisl%7==0 and first_zn%7==0):
-            #     first_chisl/=7
-            #     first_zn/=7
-            # while(first_chisl%11==0 and first_zn%11==0):
-            #     first_chisl/=11
-            #     first_zn/=11
-            # while(first_chisl%13==0 and first_zn%13==0):
-            #     first_chisl/=13
-            #     first_zn/=13
-            # while(first_chisl%17==0 and first_zn%17==0):
-            #     first_chisl/=17
-            #     first_zn/=17
-            # while(first_chisl%347==0 and first_zn%347==0):
-            #     first_chisl/=347
-            #     first_zn/=347
-            # while(first_chisl%541==0 and first_zn%541==0):
-            #     first_chisl/=541
-            #     first_zn/=541
-            # while(first_chisl%23==0 and first_zn%23==0):
-            #     first_chisl/=23
-            #     first_zn/=23  
-            # while(first_chisl%31==0 and first_zn%31==0):
-            #     first_chisl/=31
-            #     first_zn/=31  
-            # while(first_chisl%107==0 and first_zn%107==0):
-            #     first_chisl/=107
-            #     first_zn/=107
-            # while(first_chisl%1207==0 and first_zn%1207==0):
-            #     first_chisl/=1207
-            #     first_zn/=1207
-            # while(first_chisl%29791==0 and first_zn%29791==0):
-            #     first_chisl/=29791
-            #     first_zn/=29791
             k = math.gcd(first_chisl, first_zn) 
             res = str(int(first_chisl//k))+'/'+str(int(first_zn//k))
             return res
@@ -130,18 +86,18 @@ def guse(matr, row, column, row_work):
     lab1 = fractions()
     column_work=0
     while(row_work<row):
-        matrix = lab1.swap(row_work, row, column, matr,column_work)
+        matr = lab1.swap(row_work, row, column, matr,column_work)
         while(matr[row_work][column_work]=="0"):
             column_work+=1
-            matrix = lab1.swap(row_work, row, column, matr,column_work)
+            matr = lab1.swap(row_work, row, column, matr,column_work)
             if column_work == column-1:
                 return matr
             
-        delen =matrix[row_work][column_work]
+        delen =matr[row_work][column_work]
         for nums in range(row_work, column):
             matr[row_work][nums] = lab1.division(matr[row_work][nums],delen)
-        for el in matrix:
-            print(el)
+        # for el in matrix:
+        #     print(el)
         for i in range(row_work+1, row):
             mnoj= matr[i][row_work]   
             for j in range(row_work, column):
@@ -152,10 +108,10 @@ def guse(matr, row, column, row_work):
             for j in range(row_work, column):
                 param = lab1.multiplication(mnoj, matr[row_work][j])
                 matr[i][j] = lab1.subtraction(matr[i][j], param)
-        print()
-        for el in matrix:
-            print(el)
-        print()
+        #print()
+        # for el in matrix:
+        #     print(el)
+        #print()
         row_work+=1
         column_work+=1
     return matr
@@ -195,13 +151,33 @@ def prowerka_minus(num):
     else:
         num="+"+num
     return num
+def comb(n, k):
+    """Генерация сочетаний из `n` по `k` без повторений."""
+    d = list(range(0, k))
+    yield d
+    while True:
+        i = k - 1
+        while i >= 0 and d[i] + k - i + 1 > n:
+            i -= 1
+        if i < 0:
+            return
+        d[i] += 1
+        for j in range(i + 1, k):
+            d[j] = d[j - 1] + 1
+        yield d
 ### заполнение массива
 with open('matr.txt') as f:
     matrix = [list(map(str, row.split())) for row in f.readlines()]
+for el in matrix:
+    print(el)
+print()
 ######################
 row = len(matrix)
 column = len(matrix[0])
 matrix = guse(matrix, row, column, 0)  
+for el in matrix:
+    print(el)
+print()
 ### Вывод ответа
 help_m = matrix[-1]
 if check_zeros(help_m) and help_m[column-1] != "0":
@@ -209,12 +185,34 @@ if check_zeros(help_m) and help_m[column-1] != "0":
 elif row!=(column-1):
     print("Общее решение:")
     if check_zeros(help_m):
-        print_mas(matrix,0,0, column,row-1)
+        del(matrix[-1])
+        row-=1
+        print_mas(matrix,0,0, column,row)
     else:
-        
         print_mas(matrix,0,0, column,row)
     
 else:
     print_mas(matrix,0,0, column,row)
     print("Единственное решение")
 ################
+c=math.factorial(column-1)/ (math.factorial(row) *  math.factorial(column-1-row))
+print("Кол-во базисных решений: ", int(c))
+new_matr = [[str(0) for j in range(0,row+1)] for i in range(0,row)]
+
+for str in comb(column-1, row):
+        print(str)
+        for i in range(0, row):
+            for j in range(0, row):
+                column_new=str[j]
+                new_matr[i][j]=matrix[i][column_new]
+            new_matr[i][row]=matrix[i][column-1]
+        for el in new_matr:
+            print(el)
+        print()
+        
+        
+        #new_matr= guse(new_matr, row, column, 0)
+        # for el in new_matr:
+        #     print(el)
+
+
