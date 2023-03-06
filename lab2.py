ca7f4c47c5e15e5ec7bebbf688e66befe8525e39
bothy <pavla.acc@mail.ru>
2023-03-06 18:40:50 +0700

    changed lab2

--- lab2.py
+++ lab2.py
@@ -1,6 +1,29 @@
 import math
 from lab1 import *  
 ### Функция для вывод ответа
+def print_massiv(mas):
+    for el in mas:
+        print(el)
+    print()
+def find_basis(matr):
+    for i in range(0, row):
+        for j in range(0, row):
+            column_new=stroka[j]
+            matr[i][j]=matrix[i][column_new]
+            matr[i][row]=matrix[i][column-1]
+    return
+def proveraka_basisa(massive, matr_basis_str):
+    for i in range(0, len(massive)):
+        if(massive[i][0]==massive[i][1] and massive[-1]!=0):
+            print("Линейно зависимы: ")
+            print_massiv(massive) 
+            return
+    print_massiv(massive) 
+    print_mas(massive,0,0, row+1,row)
+    for i in range(row):
+        matr_basis[matr_basis_str][stroka[i]]=new_matr[i][row]
+
+    matr_basis_str+=1
 def print_mas(matr, work_column,work_row, column, row):
     if work_row == row:
         return 0
@@ -40,19 +63,16 @@ def comb(n, k):
         for j in range(i + 1, k):
             d[j] = d[j - 1] + 1
         yield d
+
 ### заполнение массива
 with open('matrix.txt') as f:
     matrix = [list(map(str, row.split())) for row in f.readlines()]
-for el in matrix:
-    print(el)
-print()
+print_massiv(matrix)
 ######################
 row = len(matrix)
 column = len(matrix[0])
 guse(matrix, row, column, 0)  
-for el in matrix:
-    print(el)
-print()
+print_massiv(matrix)
 ### Вывод ответа
 help_m = matrix[-1]
 if check_zeros(help_m) and help_m[column-1] != "0":
@@ -70,32 +90,23 @@ elif row!=(column-1):
     new_matr = [[str(0) for j in range(0,row+1)] for i in range(0,row)]
     matr_basis = [[str(0) for j in range(0,column-1)] for i in range(0,int(c))]
     matr_basis_str=0
-    for stroka in comb(column-1, row):       
-            print(stroka)
-            for i in range(0, row):
-                for j in range(0, row):
-                    column_new=stroka[j]
-                    new_matr[i][j]=matrix[i][column_new]
-                new_matr[i][row]=matrix[i][column-1]
-            for el in new_matr:
-                print(el)
-            print()        
-            new_matr =guse(new_matr, row, row+1, 0)
-            for el in new_matr:
-                print(el)
-            print()
-            print_mas(new_matr,0,0, row+1,row)
-            for i in range(row):
-
-                matr_basis[matr_basis_str][stroka[i]]=new_matr[i][row]
-            matr_basis_str+=1
-            #print(new_matr)
     print("\n Базисные решения: ")
-    new_i=len(matr_basis[0])
-    for el in range(0, len(matr_basis)):
-        for el_i in range(0,new_i):
-            print("x",el_i+1," = ", matr_basis[el][el_i])
-        print()
+    for stroka in comb(column-1, row): 
+            print("##############################")      
+            print("\t",stroka,"\t")
+            find_basis(new_matr)        
+            print_massiv(new_matr)      
+            new_matr =guse(new_matr, row, row+1, 0)
+            print_massiv(new_matr)
+            #print_mas(new_matr,0,0, row+1,row)
+            #proveraka_basisa(new_matr,matr_basis_str)
+            
+    # print("\n Базисные решения: ")
+    # new_i=len(matr_basis[0])
+    # for el in range(0, len(matr_basis)):
+    #     for el_i in range(0,new_i):
+    #         print("x",el_i+1," = ", matr_basis[el][el_i])
+    #     print()
 
     
 else:
